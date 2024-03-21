4aad7c8813fdf99c9a1e56b250189f764545ce5b
9NANI9 <elite.dag@gmail.com>
2024-03-22 01:39:43 +0300

    add apoj

---
+++ Store/JS/cart.js
@@ -0,0 +1,21 @@
+import Product from './Product.js'
+
+let product = new Product()
+
+const massivCopy = product.get_cart()
+for (let item of massivCopy) {
+  product.create(item.name, item.price, item.currancy, item.img);
+}
+const cost=product.count_total(massivCopy)
+console.log(cost)
+
+const labelDollar = document.querySelector('label[for="price-dollar"]');
+labelDollar.textContent = cost.dollar+" $";
+
+// Изменение значения метки для евро
+const labelEuro = document.querySelector('label[for="price-euro"]');
+labelEuro.textContent = cost.euro+"€ ";
+
+// Изменение значения метки для рубля
+const labelRuble = document.querySelector('label[for="price-ruble"]');
+labelRuble.textContent = cost.ruble+"₽ ";
